3e06ef3e8686f7a9bb6b58d18bca2c2e681b1313
KaungMyatThu30 <kgmyatthu10969@email.com>
2025-12-26 15:32:29 +0700

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import Register from "./components/Register.jsx";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Register />} />
+      <Route path="/deployRegisterForm/" element={<Register />} />
+    </Routes>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReatDCM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+
+ReatDCM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
